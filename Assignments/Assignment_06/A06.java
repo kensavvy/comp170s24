ee4d23e92b086c0ad4cb37f635d94c71c5c85dad
kensavvy <ksavino1@luc.edu>
2024-07-10 18:47:00 +0000

    homework6 commit 1

--- Assignments/Assignment_06/A06.java
+++ Assignments/Assignment_06/A06.java
@@ -1,5 +1,5 @@
-import java.io.FileNotFoundException;
-import java.io.File;
+//import java.io.FileNotFoundException;
+//import java.io.File;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -13,7 +13,43 @@ public class A06 {
         /**********************************************************************
          *                       WRITE YOUR CODE HERE                         *
          *********************************************************************/
-        // Close the scanner
-        sc.close(); 
+        int guessNumber = -1;
+        // initialize user guess
+    
+        while(guessNumber != randomNumber && guessNumber != -1) {
+            System.out.println("Guess a number between 0 and 99. Enter -1 to quit: "); 
+        // ask user to enter integer number or terminate program
+
+            if(sc.hasNextInt()) {
+                guessNumber = sc.nextInt();
+            // read user guess
+
+                if(guessNumber == -1) {
+                    System.out.println("You have quit the game.");
+                // user enters -1 to quit the game
+
+                } else if(guessNumber < 0 || guessNumber > 99) {
+                    System.out.println("You have guessed a number out of bounds. Please enter an integer from 0 to 99.");
+                // user guesses a number that is not between 0 and 99
+
+                } else if(guessNumber < randomNumber) {
+                    System.out.println("You have guessed a number too small. Try again!");
+                // user guesses a number that is smaller than the random number
+
+                } else if(guessNumber > randomNumber) {
+                    System.out.println("You have guessed a number too large. Try again!");
+                // user guesses a number that is larger than the random number
+
+                } else {
+                    System.out.println("You have guessed the correct number! Hooray!");
+                // user guesses the random number
+                }
+
+            } else {
+                System.out.println("You have guessed an invalid input. Please enter an integer from 0 to 99.");
+            // user guesses a non-integer input
+            }
+        }
+        sc.close(); // Close the scanner
     } // method main
 }
